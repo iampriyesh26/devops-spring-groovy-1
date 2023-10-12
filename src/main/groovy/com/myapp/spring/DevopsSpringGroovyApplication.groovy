0de80cb6cb52a4b20514b65e85e3173d36bd01b9
PriyeshDutta <duttapriyesh@gmail.com>
2023-10-12 23:51:31 +0530

    Adding file to remote repo

---
+++ src/main/groovy/com/myapp/spring/DevopsSpringGroovyApplication.groovy
@@ -0,0 +1,14 @@
+package com.myapp.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+//some comments
+@SpringBootApplication
+class DevopsSpringGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DevopsSpringGroovyApplication, args)
+	}
+
+}
